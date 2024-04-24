c535a8bb57c47c19bd1250683e292248933a152d
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-04-24 16:12:15 -0500

    Commit

--- Semana_01/Semana_01/Program.cs
+++ Semana_01/Semana_01/Program.cs
@@ -241,52 +241,7 @@ namespace Semana_01
 
             }while (optionGeneral !="7");
 
-            /*
-            string opcion;
-            do
-            {
-                Console.WriteLine("Elija qué figura desea operar:");
-                Console.WriteLine("1. Rectángulo");
-                Console.WriteLine("2. Círculo");
-                Console.WriteLine("3. Triángulo");
-                Console.WriteLine("4. Salir");
-
-                opcion = Console.ReadLine();
-
-                switch (opcion)
-                {
-                    case "1":
-                        Console.WriteLine("Ingrese la base del rectángulo:");
-                        float baseRectangulo = float.Parse(Console.ReadLine());
-                        Console.WriteLine("Ingrese la altura del rectángulo:");
-                        float alturaRectangulo = float.Parse(Console.ReadLine());
-                        Rectangulo rectangulo = new Rectangulo(baseRectangulo, alturaRectangulo);
-                        Console.WriteLine("El área del rectángulo es: " + rectangulo.CalcularArea());
-                        break;
-                    case "2":
-                        Console.WriteLine("Ingrese el radio del círculo:");
-                        float radio = float.Parse(Console.ReadLine());
-                        Circulo circulo = new Circulo(radio);
-                        Console.WriteLine("El área del círculo es: " + circulo.CalcularArea());
-                        break;
-                    case "3":
-                        Console.WriteLine("Ingrese la base del triángulo:");
-                        float baseTriangulo = float.Parse(Console.ReadLine());
-                        Console.WriteLine("Ingrese la altura del triángulo:");
-                        float alturaTriangulo = float.Parse(Console.ReadLine());
-                        Triangulo triangulo = new Triangulo(baseTriangulo, alturaTriangulo);
-                        Console.WriteLine("El área del triángulo es: " + triangulo.CalcularArea());
-                        break;
-                    case "4":
-                        Console.WriteLine("¡Adiós!");
-                        break;
-                    default:
-                        Console.WriteLine("Opción no válida. Por favor, seleccione una opción válida.");
-                        break;
-                }
-
-            } while (opcion != "4");
-            */
+           
         }
     }
 }
